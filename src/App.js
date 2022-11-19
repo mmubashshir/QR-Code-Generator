8be8b166a48de65ddf07f9358ec0d7e07217fa23
mmubashshir <mirmubashshir@gmail.com>
2022-11-19 23:12:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Qrcode from './components/qrcode';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState();
+  function getUrl(event){
+    setData(event.target.value);
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <Qrcode url={data}/>
+        <div className='textArea'>
+            <div className='heading'>Generate QR code for your url</div>
+            <div className='inputUrl'>
+                <input className='urlInput' type='url' placeholder='Enter any URL or Text' onChange={getUrl}/>
+            </div>
+            <div className='note'>Scan the QR Code to go to the URL and share with friends</div>
+        </div>
+    </main>
     </div>
   );
 }
